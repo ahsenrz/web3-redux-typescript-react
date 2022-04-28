914ad7dc4e027dc9f7dcb3974ec318981205d9b7
ahsenrz <>
2022-04-28 10:56:54 +0500

    Moved frontend folder

--- frontend/src/pages/about/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AboutScreen = () => {
-  return (
-    <div>This project is related to minting nft</div>
-  )
-}
-
-export default AboutScreen
--- frontend/src/redux/parentReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { web3Reducer } from "./slices/web3ConnectSlice";
-
-const parentReducer = combineReducers({
-  web3Connect: web3Reducer,
-});
-
-export default parentReducer;
